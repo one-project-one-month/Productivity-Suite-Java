2e71b8636a92b52f55b9d998f81e9d7f3ac4e101
Thi Han Linn <thihanlinn1003@gmail.com>
2025-05-13 16:01:45 +0630

    fix for userId

--- src/main/java/com/_p1m/productivity_suite/features/note_taking/dto/NoteResponse.java
+++ src/main/java/com/_p1m/productivity_suite/features/note_taking/dto/NoteResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NoteResponse {
+    private Long userId;
     private Long id;
     private String title;
     private String body;
