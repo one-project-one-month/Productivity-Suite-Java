b358bbf7544aa7d182684adaa18ed0e6d1252d9c
Thant Htoo Aung <thanthtoo1285@gmail.com>
2025-05-15 22:12:42 +0700

    fixed annotation in category request

--- src/main/java/com/_p1m/productivity_suite/features/categories/dto/CategoryRequest.java
+++ src/main/java/com/_p1m/productivity_suite/features/categories/dto/CategoryRequest.java
@@ -1,5 +1,6 @@
 package com._p1m.productivity_suite.features.categories.dto;
 
+import com._p1m.productivity_suite.config.annotations.ValidCategoryDescription;
 import com._p1m.productivity_suite.config.annotations.ValidCategoryType;
 import com._p1m.productivity_suite.config.annotations.ValidName;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,7 @@ public class CategoryRequest {
     @ValidName
     private String name;
 
-    @ValidCategoryType
+    @ValidCategoryDescription
     private String description;
 
     @ValidCategoryType
