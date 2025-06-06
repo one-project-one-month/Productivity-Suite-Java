69e34eaaae6c03c9d8115487a75485c21a0973cd
thanthtooaung <thanthtoo1285@gmail.com>
2025-06-06 16:03:43 +0700

    fix createdAt

--- src/main/java/com/_p1m/productivity_suite/features/income/service/impl/IncomeServiceImpl.java
+++ src/main/java/com/_p1m/productivity_suite/features/income/service/impl/IncomeServiceImpl.java
@@ -48,7 +48,7 @@ public class IncomeServiceImpl implements IncomeService {
         final UserDto userDto = this.userUtil.getCurrentUserDto(authHeader);
         final List<Income> incomes = RepositoryUtils.findAllByUserId(
                 userDto.getId(),
-                Sort.by(Sort.Direction.ASC, "created_at"),
+                Sort.by(Sort.Direction.ASC, "createdAt"),
                 this.incomeRepository::findAllByUserId
         );
         return incomes.stream()
